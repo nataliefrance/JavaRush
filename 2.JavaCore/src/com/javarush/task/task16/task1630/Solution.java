1c5a125bebf004f945dd82fb298fb4e3b98709b4
Natalia Shipova <nataliefrance@mail.ru>
2019-02-22 17:53:04 +0300

    Дописала задачку

--- 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -17,7 +17,6 @@ public class Solution {
         }
     }
 
-
     public static void main(String[] args) throws IOException, InterruptedException {
         systemOutPrintln(firstFileName);
         systemOutPrintln(secondFileName);
