472322c5382079c86b0c458f6346f7beeee84232
Natalia Shipova <nataliefrance@mail.ru>
2019-05-30 21:36:17 +0300

    added task 1801

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1802;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Минимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        FileInputStream fis = new FileInputStream(fileName);
+
+        int min = Byte.MAX_VALUE;
+
+        while (fis.available() > 0) {
+            int i = fis.read();
+            if (i < min) {
+                min = i;
+            }
+        }
+        System.out.println(min);
+        fis.close();
+    }
+}
